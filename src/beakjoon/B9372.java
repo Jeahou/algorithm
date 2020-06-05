4676c14160a68d0e1a20cbe40bf44e69c65a3f7d
note <note@DESKTOP-STBPJ8E>
2020-06-05 16:53:39 +0900

    init commit //

---
+++ src/beakjoon/B9372.java
@@ -0,0 +1,21 @@
+package beakjoon;
+import java.util.*;
+
+public class B9372 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int test_case = sc.nextInt();
+		for(int i=0; i<test_case; i++) {
+			int countryN = sc.nextInt();
+			int planeN = sc.nextInt();
+			for(int j=0; j<planeN; j++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+			}
+			System.out.println(countryN-1);
+		}
+	}
+
+}
